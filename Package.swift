8129c4b84cb052fff18dcfb3bba7f8c1dec76b4d
Ethan Wong <e1hanW0ng@gmail.com>
2021-09-18 11:21:49 +0800

    Update for Xcode 12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "GSMessages",
   platforms: [
-    .iOS(.v8),
+    .iOS(.v9),
   ],
   products: [
     .library(name: "GSMessages", targets: ["GSMessages"])
